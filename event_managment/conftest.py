d121911cc40856eb45598562968659a583c11a07
Abdul Rehman Khalid <abdulrehmanshafi069@gmail.com>
2023-08-18 19:17:01 +0500

    Initial Commit

---
+++ event_managment/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from event_managment.users.models import User
+from event_managment.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
